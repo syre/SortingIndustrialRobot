720fc4cc7abda81164f7dc00864719d2c5cb3274
Søren Howe Gersager <syrelyre@gmail.com>
2012-02-26 21:34:23 +0100

    tilføjet script for converting component

---
+++ Database/Converting component/ConvertingComponentCRUD.sql
@@ -0,0 +1,28 @@
+-- revision 1.0
+-- mangler at tilføje READ, UPDATE og DELETE til Position table mm.
+
+-- CREATE
+CREATE TABLE BoxDimensions
+(BoxID int PRIMARY KEY NOT NULL IDENTITY,
+Length float,
+Width float,
+Depth float)
+
+CREATE TABLE Position
+(BoxID int REFERENCES BoxDimensions(BoxID),
+Angle int,
+Distance float)
+
+INSERT BoxDimensions ([Length],[Width],[Depth]) VALUES (100.0,200.0,300.0)
+
+-- READ
+SELECT * FROM BoxDimensions
+
+-- UPDATE
+UPDATE BoxDimensions
+SET [Length]=200,[Width]=300,[Depth]=400
+WHERE BoxID=1
+
+-- DELETE
+DELETE FROM BoxDimensions
+WHERE BoxID=1
