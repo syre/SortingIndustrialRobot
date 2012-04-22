f85d7dfbdac4b9315c68d9d3cbdc87a2ac5c1f1a
Cong Thanh Dao <thanh_dc@hotmail.com>
2012-04-22 22:09:20 +0200

    MVVM af Gem og åben prøvet at lave test til relaycommands

--- Kode/SIR/RoboGO/MainWindow.xaml.cs
+++ Kode/SIR/RoboGO/MainWindow.xaml.cs
@@ -14,6 +14,7 @@ using System.Windows.Shapes;
 using System.Xml.Serialization;
 using GalaSoft.MvvmLight.Command;
 using Microsoft.Win32;
+using Command;
 
 
 namespace RoboGO
@@ -24,28 +25,12 @@ namespace RoboGO
     public partial class MainWindow : Window
     {
 
-
-        public RelayCommand SaveAs { get; private set; }
-        public RelayCommand Open { get; private set; }
-        public RelayCommand CloseTab { get; private set; }
-
+        
         public MainWindow()
         {
             InitializeComponent();
 
-            SaveAs = new RelayCommand(
-                () => SaveAs_Executed(),
-                () => SaveAs_CanExecute);
-
-            Open = new RelayCommand(
-                () => Open_Executed(),
-                () => Open_CanExecute);
-
-            CloseTab = new RelayCommand(
-                () => CloseTab_Executed(),
-                () => CloseTab_CanExecute);
-
-            mnuFile1.DataContext = this;
+            mnuFile1.DataContext = new InOutService();
         }
 
         // A function within main that invokes function DisplayLogin
@@ -66,77 +51,6 @@ namespace RoboGO
             else
                 this.Close();
         }
-
-        #region CommandHandlers
-
-        protected bool SaveAs_CanExecute
-        {
-            get { return (IDETabs.SelectedIndex >= 0); }
-        }
-
-        private void SaveAs_Executed()
-        {
-            SaveFileDialog saveDialog = new SaveFileDialog();
-            saveDialog.Filter = "TXT Files(*.txt)|*.txt";
-            saveDialog.DefaultExt = "xml";
-            saveDialog.AddExtension = true;
-
-            if (saveDialog.ShowDialog() == true)
-            {
-                StreamWriter writer = new StreamWriter(saveDialog.OpenFile());
-                TextBox tempBox = (TextBox)((TabItem)IDETabs.Items[0]).Content;
-                writer.Write(tempBox.Text);
-                writer.Close();
-            }
-        }
-
-        protected bool Open_CanExecute
-        {
-            get { return true; }
-        }
-
-        private void Open_Executed()
-        {
-            OpenFileDialog openDialog = new OpenFileDialog();
-            openDialog.Filter = "TXT Files(*.txt)|*.txt";
-            openDialog.CheckFileExists = true;
-            openDialog.Multiselect = true;
-
-            if (openDialog.ShowDialog() == true)
-            {
-
-                foreach (string file in openDialog.FileNames)
-                {
-                    Stream tempStream = new FileStream(file, FileMode.Open, FileAccess.Read, FileShare.ReadWrite);
-                    StreamReader tempReader = new StreamReader(tempStream);
-
-                    TabItem newTab = new TabItem();
-                    TextBox tempBox = new TextBox();
-                    tempBox.VerticalScrollBarVisibility = ScrollBarVisibility.Auto;
-                    tempBox.Text = tempReader.ReadToEnd();
-
-                    newTab.Content = tempBox;
-                    FileInfo fi = new FileInfo(file);
-                    newTab.Header = fi.Name;
-                    IDETabs.Items.Add(newTab);
-                    tempReader.Close();
-                }
-            }
-
-        }
-
-        protected bool CloseTab_CanExecute
-        {
-            get { return (IDETabs.SelectedIndex >= 0); }
-        }
-
-        private void CloseTab_Executed()
-        {
-            IDETabs.Items.Remove(IDETabs.SelectedItem);
-        }
-
-        #endregion
-
 
     }
 }
