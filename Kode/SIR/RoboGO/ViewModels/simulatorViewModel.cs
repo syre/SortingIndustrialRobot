ba98bec3173ec6a78836ef8ffe7d265198a4f0d0
10568@iha.dk <yusuf251085>
2012-05-18 11:51:53 +0200

    Simulator added

--- Kode/SIR/RoboGO/ViewModels/simulatorViewModel.cs
+++ Kode/SIR/RoboGO/ViewModels/simulatorViewModel.cs
@@ -1,9 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using ControlSystem;
 
-namespace DSL
+
+namespace RoboGO.ViewModels
 {
     public class simulatorViewModel
     {
