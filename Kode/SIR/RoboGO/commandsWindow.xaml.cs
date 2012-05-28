be6675d64a8072dbb9b81d14bdf34877e57e0bcc
Søren Howe Gersager <syrelyre@gmail.com>
2012-05-28 12:51:52 +0200

    opdateret commandswindow med rette sti til commands.html fil

--- Kode/SIR/RoboGO/commandsWindow.xaml.cs
+++ Kode/SIR/RoboGO/commandsWindow.xaml.cs
@@ -16,7 +16,8 @@ namespace RoboGO
 
         public void NavigateToHelp()
         {
-            helpbrowser.Navigate(new Uri("commands.html"));
+            helpbrowser.Navigate(new Uri(String.Format("file:///{0}/DSLFiler/commands.html"
+                                                       , Directory.GetCurrentDirectory())));
         }
     }
 }
